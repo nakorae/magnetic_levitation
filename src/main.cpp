2992e6b28ce00d8743c2d285c82a1d53d3194580
nakorae <mugen9su@icloud.com>
2023-09-15 10:13:49 +0900

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+
+#define inpin 0
+#define outpin 10
+
+int val;
+float u;
+float e;
+float pre_e = 0;
+
+void setup()
+{
+  pinMode(inpin, INPUT);
+  pinMode(outpin, OUTPUT);
+  TCCR1B = 1;
+}
+
+void loop()
+{
+  val = analogRead(inpin);
+  pre_e = e;
+  e = 400 - val;
+  u = 4 * (e + 10 * (e - pre_e));
+  if (u > 255)
+    u = 255;
+  if (u < 0)
+    u = 0;
+  analogWrite(outpin, u);
+
+  delayMicroseconds(300);
+}
